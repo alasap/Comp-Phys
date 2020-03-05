b69bcbaeff6f17cc8729a65988e2020c9dea0045
alasap <ahall24@cub.uca.edu>
2020-03-05 10:38:44 -0600

    Adding files already made to my initial Computational Physics local repsoitory

---
+++ 2.12.py
@@ -0,0 +1,14 @@
+#Program for caclulating all of the prime numbers below a specified limit.
+primes=[2]
+lim=10000
+n=3
+while n<lim:
+    for i in primes:
+        if n%i==0:
+            bool='false'
+            break
+        else:bool='true'
+    if bool=='true':
+        primes.append(n)
+    n=n+1
+print(primes)
