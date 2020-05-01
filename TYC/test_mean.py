abb34d681c3446753cabccb8c13544abd07c9b66
Austin <ahall24@cub.uca.edu>
2020-05-01 14:15:13 -0500

    okay now im adding a second test

--- TYC/test_mean.py
+++ TYC/test_mean.py
@@ -7,6 +7,9 @@ class TestCode(unittest.TestCase):
         fun = mean([1,1])
         exp = 1
         self.assertEqual(fun, exp)
-
+    def test_mean1(self):
+        fun = mean([2,2])
+        exp = 2
+        self.assertEqual(fun ,exp)
 if __name__ == '__main__':
     unittest.main()
